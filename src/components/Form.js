559236c86e870cc5b3e8f0ef27069f6eece1882a
Rafid <faiyazrafid5@gmail.com>
2022-03-30 23:37:45 +0600

    Rafid-Part

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
 // This is Rafid's part
-import '../Rafid.css';
+import "../Rafid.css";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div className="form">
+      <form>
+        <input
+          type="text"
+          className="search-input"
+        />
+        <button type="submit" className="submit-btn">Calculate</button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
