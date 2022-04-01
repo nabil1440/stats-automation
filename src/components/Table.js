5e24eb80f661785b580f63932f7ac1b95a216262
Atique Shahriar <satique06@gmail.com>
2022-04-01 12:01:20 +0600

    Frequency Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React from "react";
 
 // This is Atique's part
-import '../Atique.css';
+import "../Atique.css";
 
 const Table = () => {
-  return <div>Table</div>;
+  return (
+    <div>
+      <table>
+        <caption>Frequency Distribution Table</caption>
+        <tr>
+          <th>Class</th>
+          <th>Frequency</th>
+          <th>Midpoint</th>
+          <th>Cumulative Frequency</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
