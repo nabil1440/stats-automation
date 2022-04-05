90fc43d192fd01ba3b36d0a0bfe35024421d6856
Ihsan Ahmad <nahidboss96@gmail.com>
2022-04-05 20:00:17 +0600

    Finally,,,,,,

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,11 @@ import '../Ihsan.css';
 
 const Header = () => {
   return (
-    <div className='red'>
-      Header
+    <div className='header'>
+      <div id="text">
+        <a href="#home">TABLE</a>
+        <a href="#home">CENTRAL TENDENCIES</a>
+      </div>
       {/* Link Example: 
       <Link to='/results'>Results Page</Link> */}
     </div>
--- src/components/MMMode.js
+++ src/components/MMMode.js
@@ -4,7 +4,34 @@ import React from 'react';
 import '../Ihsan.css';
 
 const MMMode = () => {
-  return <div>MMMode</div>;
+  return (
+  <div className='row'>
+    <div className='box'>
+      <div className='text_M'>
+        MEAN
+      </div>
+      <div className='value'>
+        
+      </div>
+    </div>
+    <div className='box'>
+      <div className='text_M'>
+        MEDIAN
+      </div>
+      <div className='value'>
+
+      </div>
+    </div>
+    <div className='box'>
+      <div className='text_M'>
+        MODE
+      </div>
+      <div className='value'>
+        
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default MMMode;
